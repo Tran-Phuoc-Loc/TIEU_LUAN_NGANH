8da360b7c2cc2cc376c6fb525534995c4a2b3ae5
Tran Phuoc Loc <tranphuocloctinphat@gmail.com>
2024-08-09 14:29:24 +0700

    sửa giao diện

--- forum/resources/views/welcome.blade.php
+++ forum/resources/views/welcome.blade.php
@@ -17,12 +17,15 @@
         <header class="p-3">
             <nav class="navbar navbar-expand-lg navbar-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="{{ url('/') }}">Home</a>
+                    <a class="navbar-brand" href="{{ url('/') }}"><img src="{{ asset('storage/images/bookicon.png') }}" alt="Description">TechTalks</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <ul class="navbar-nav">
+                            <li>
+                                <a class="nav-link" href="{{ url('/') }}">Home</a>
+                            </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('posts.index') }}">Posts</a>
                             </li>
